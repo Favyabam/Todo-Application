84df1bc2405481369edc4a7f4bd740029241f23d
Favyabam <favourdickens82@yahoo.com>
2025-09-03 17:57:28 +0100

    new feature: initial commit

---
+++ src/assets/assets.js
@@ -0,0 +1,11 @@
+import delete_icon from "./delete_icon.png";
+import not_tick from "./not_tick.png";
+import tick from "./tick.png";
+import todo_icon from "./todo_icon.png";
+
+export const assets = {
+    delete_icon,
+    not_tick,
+    tick,
+    todo_icon
+};
